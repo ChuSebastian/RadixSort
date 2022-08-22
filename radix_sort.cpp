a24b4e54e005db32860b21421de783b2ba1a7e31
ChuSebastian <sebastian.chu@utec.edu.pe>
2022-08-21 20:55:28 -0500

    updated time unit to nanoseconds

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -63,7 +63,7 @@ int main() {
     radixSort(arr, n);
     auto stop = high_resolution_clock::now();
 
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    cout << "Time taken by function: " << duration.count() << " nanoseconds" << endl;
     return 0;
 }
